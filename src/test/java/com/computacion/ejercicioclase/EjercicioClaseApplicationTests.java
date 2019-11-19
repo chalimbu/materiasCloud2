c553d671e037d3fcdeb3556f16ce444d21368abe
sala 205 <sebastian_zapata82151@elpoli.edu.co>
2019-11-18 19:00:09 -0500

    first commit

---
+++ src/test/java/com/computacion/ejercicioclase/EjercicioClaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computacion.ejercicioclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioClaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
